de476f193fddf4e0cd4b12044a9a087d077cca14
Toxirov Bobur <tokhirovbob@gmail.com>
2023-09-03 14:47:34 +0500

    add macros

--- 00_basics/01_macros.c
+++ 00_basics/01_macros.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
 // ushbu macros qiymati preprocessor tomonidan o'qib olinadi
 #define PI 3.1415 
+#define Area(l,b) (l * b)
 
-int main()
-{
+void macros_one() {
     float radius, area;
     printf("Enter the radius: ");
     scanf("%f", &radius);
-
     area = PI*radius*radius;
 
-    printf("Area=%.2f",area);
+    printf("Area=%.2f \n",area);
+    return;
+}
+
+void macros_two() {
+    int length = 15, breadth = 20, rect_area;
+    rect_area = Area(length,breadth);
+    printf("Area of the rectangle : %d \n",rect_area);
+    return;
+}
+
+void macros_standart() {
+    printf("Current File :%s\n", __FILE__);
+    printf("Current Date :%s\n", __DATE__);
+    printf("Current Time :%s\n", __TIME__);
+    printf("Line Number :%d\n", __LINE__);
+    return;
+}
+
+int main()
+{
+    macros_one();
+    macros_two();
+    macros_standart();
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # learning-c-lang
+
+urls
+https://www.algbly.com/Tutorials/C-programming/C-course.html
+https://www.programiz.com/c-programming

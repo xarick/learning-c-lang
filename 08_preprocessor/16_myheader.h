02f0a85a6fccb7fac3dad4f7889d4474e3cae993
Bobirjon Tohirov <tokhirovbob@gmail.com>
2025-01-03 09:36:33 +0500

    header files added

---
+++ 08_preprocessor/16_myheader.h
@@ -0,0 +1,13 @@
+// myheader.h
+
+#ifndef MYHEADER_H   // Agar MYHEADER_H aniqlanmagan bo'lsa
+#define MYHEADER_H   // MYHEADER_H ni aniqlash
+
+// Konstantalar
+#define PI 3.14159
+
+// Funksiya prototipi
+int add(int a, int b);
+double area_of_circle(double radius);
+
+#endif // MYHEADER_H
